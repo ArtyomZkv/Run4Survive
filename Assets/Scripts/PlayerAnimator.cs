f4949490523e231b8aa3158e2acd545fe8d8cd32
Artyom <zujkov.ks52@gmail.com>
2025-06-24 19:06:31 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,45 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    //References
+    Animator animator;
+    PlayerMovement playerMovement;
+    SpriteRenderer spriteRenderer;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();    
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMovement.moveDir.x !=0 || playerMovement.moveDir.y != 0)
+        {
+            animator.SetBool("Move", true);
+
+            SpriteDirectionChecker();
+        }
+        else
+        {
+            animator.SetBool("Move", false);
+        }
+    }
+    void SpriteDirectionChecker()
+    {
+        if(playerMovement.lastHorizontalVector < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX= false;
+        }
+    }
+}
