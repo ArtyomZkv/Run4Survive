3822cd126352be901a8a6b203853490cc04adf00
Artyom <zujkov.ks52@gmail.com>
2025-07-08 02:50:44 +0300

    new weapon class and sprite sheet
    Add Weapon sprite Sheet
    Create parent class WeaponController for override functions for each weapon

---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,36 @@
+using Unity.Hierarchy;
+using UnityEngine;
+
+/// <summary>
+/// Base script for all weapons
+/// </summary>
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public GameObject prefab;
+    public float damage;
+    public float speed;
+    public float cooldownDuration;
+    float currentCooldown;
+    public int pierce;
+
+    protected virtual void Start()
+    {
+        currentCooldown = cooldownDuration; // At the start set the current cooldown to be the cooldown duration
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown <= 0f)
+        {
+            Attack();
+        }
+    }
+
+   protected virtual void Attack()
+    {
+        currentCooldown = cooldownDuration;
+    }
+}
