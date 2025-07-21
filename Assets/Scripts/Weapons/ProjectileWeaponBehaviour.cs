1d2b3aa381ca6c24aad9060b75a64ea532684949
Artyom <zujkov.ks52@gmail.com>
2025-07-21 17:41:51 +0300

    Changed the rotation system of projectileWeapon

--- Assets/Scripts/Weapons/ProjectileWeaponBehaviour.cs
+++ Assets/Scripts/Weapons/ProjectileWeaponBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -20,5 +21,14 @@ public class ProjectileWeaponBehaviour : MonoBehaviour
         this.direction = direction;
         this.speed = speed;
     }
-   
+    public void DirectionChecker()
+    {
+        float baseAngleOffset = -45f;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Vector3 scale = transform.localScale;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + baseAngleOffset);
+        transform.localScale = scale;
+    }
 }
